b0480601d4d5dc61acc3e9b521a16c8dcd4e4979
leonit27 <leonit@alu.edu.gva.es>
2024-12-10 13:58:35 +0100

    Mejor terminación con métodos y frequency acabado

---
+++ EstructurasDeDatos/src/Frequency.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Frequency {
+    public static String pedirPalabras(Scanner s) {
+        System.out.print("Introduce las palabras separadas por comas: ");
+        return s.nextLine();
+    }
+
+    public static void rellenarArrayList(ArrayList<String> palabras, String boletosIntroducidos) {
+        //Separamos las palabras introducidas separadas por comas y los metemos en una array
+        String[] partes = boletosIntroducidos.split(",");
+
+        //Recorremos la array "partes" y añadimos todos los elementos a la ArrayList palabras
+        for (String parte : partes) {
+            palabras.add(parte.trim()); //El metodo trim sirve para eliminar espacios innecesarios entre las comas
+        }
+    }
+
+    public static void rellenarMapa(ArrayList<String> palabras, Map<String, Integer> repeticiones) {
+        for (String palabra : palabras) {
+            //Actualizamos el Map con la clave (palabra) e incrementamos su valor asociado por cada vez que se repita la palabra
+            repeticiones.put(palabra, repeticiones.getOrDefault(palabra, 0) + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        //Creamos la ArrayList que contendrá las palabras introducidas
+        ArrayList<String> palabras = new ArrayList<>();
+
+        //Creamos el mapa que contendrá cuantas veces se repite una palabra
+        Map<String, Integer> repeticiones = new HashMap<>();
+
+        //Creamos el Scanner
+        Scanner s = new Scanner(System.in);
+
+        //Pedimos las palabras
+        String palabrasIntroducidas = pedirPalabras(s);
+
+        //Cerramos el Scanner
+        s.close();
+
+        //Metemos las palabras introducidas en la ArrayList
+        rellenarArrayList(palabras, palabrasIntroducidas);
+
+        //Actualizamos el mapa con las terminaciones y el número de veces que se repiten
+        rellenarMapa(palabras, repeticiones);
+
+        //Mostramos el mapa
+        System.out.println(repeticiones);
+    }
+}
--- EstructurasDeDatos/src/MejorTerminacion.java
+++ EstructurasDeDatos/src/MejorTerminacion.java
@@ -4,36 +4,54 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class MejorTerminacion {
+    public static String pedirBoletos(Scanner s) {
+        System.out.print("Introduce los números de los boletos separados por comas (deben tener 5 cifras): ");
+        return s.nextLine();
+    }
+
     public static void rellenarArrayList(ArrayList<String> boletos, String boletosIntroducidos) {
         //Separamos los boletos introducidos por comas y los metemos en una array
         String[] partes = boletosIntroducidos.split(",");
 
-        //Recorremos la array "partes" y añadimos, convirtiendo en números, todos los elementos a la ArrayList boletos
+        //Recorremos la array "partes" y añadimos todos los elementos a la ArrayList boletos
         for (String parte : partes) {
             boletos.add(parte.trim()); //El metodo trim sirve para eliminar espacios innecesarios entre las comas
         }
     }
-    
-    public static void main(String[] args) {
-        ArrayList<String> boletos = new ArrayList<>();
 
-        Map<Integer, Integer> terminaciones = new HashMap<>();
-
-        Scanner s = new Scanner(System.in);
-
-        System.out.print("Introduce los números de los boletos separados por comas (deben tener 5 cifras): ");
-        String boletosIntroducidos = s.nextLine();
-
-        rellenarArrayList(boletos, boletosIntroducidos);
-
-        System.out.println(boletos);
-
-        int ultimoDigito;
+    public static void rellenarMapa(ArrayList<String> boletos, Map<Integer, Integer> terminaciones) {
+        int ultimoDigito; //Creamos la variable para almacenar el último dígito
 
         for (String boleto : boletos) {
-            ultimoDigito = Character.getNumericValue(boleto.charAt(4));
+            ultimoDigito = Character.getNumericValue(boleto.charAt(4)); //El metodo getNumericValue sirve para convertir un carácter a su valor numérico equivalente
+            //Actualizamos el Map con la clave (ultimoDigito) e incrementamos su valor asociado
             terminaciones.put(ultimoDigito, terminaciones.getOrDefault(ultimoDigito, 0) + 1);
         }
+    }
+    
+    public static void main(String[] args) {
+        //Creamos la ArrayList que contendrá los boletos introducidos
+        ArrayList<String> boletos = new ArrayList<>();
+
+        //Creamos el mapa que contendrá cuantas veces se repite una terminación
+        Map<Integer, Integer> terminaciones = new HashMap<>();
+
+        //Creamos el Scanner
+        Scanner s = new Scanner(System.in);
+
+        //Pedimos los boletos
+        String boletosIntroducidos = pedirBoletos(s);
+
+        //Cerramos el Scanner
+        s.close();
+
+        //Metemos los boletos introducidos en la ArrayList
+        rellenarArrayList(boletos, boletosIntroducidos);
+
+        //Actualizamos el mapa con las terminaciones y el número de veces que se repiten
+        rellenarMapa(boletos, terminaciones);
+
+        //Mostramos el mapa
         System.out.println(terminaciones);
     }
 }
