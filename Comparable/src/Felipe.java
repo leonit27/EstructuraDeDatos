17b9b0528ff9234faeb2eeb1ce9f986d268413cc
Leonardo <leonit@alu.edu.gva.es>
2025-01-07 20:41:07 +0100

    Felipe sin acabar

---
+++ Comparable/src/Felipe.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Felipe {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String linea;
+
+        do {
+            linea = s.nextLine();
+        } while (!linea.equals("0"));
+    }
+}
