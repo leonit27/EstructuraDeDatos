cf79f06458b94d9dfe85b374fc2f7159b5577be4
Leonardo <leonit@alu.edu.gva.es>
2025-01-08 20:32:58 +0100

    Empezando pilas

--- Pilas/src/Paréntesis.java
+++ Pilas/src/Paréntesis.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Paréntesis {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Stack stack1 = new Stack();
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Introduce una cadena parentizada: ");
+        String cadena = s.nextLine();
+
+        for (int i = 0; i < cadena.length(); i++) {
+            if (cadena.charAt(i) == '(') stack1.push('(');
+            if (cadena.charAt(i) == ')') {
+                //stack1.push(')');
+                stack1.search(')');
+            }
+        }
+
+        System.out.println(stack1);
     }
 }
