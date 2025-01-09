81d12ede44bfa449060bca06cd798a98f84792c6
Leonardo <leonit@alu.edu.gva.es>
2025-01-09 20:21:29 +0100

    Paréntesis acabado y Felipe copiado

--- Pilas/src/Paréntesis.java
+++ Pilas/src/Paréntesis.java
@@ -2,22 +2,51 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class Paréntesis {
-    public static void main(String[] args) {
-        Stack stack1 = new Stack();
-
-        Scanner s = new Scanner(System.in);
-
+    public static String introducirCadena(Scanner s) {
         System.out.print("Introduce una cadena parentizada: ");
-        String cadena = s.nextLine();
+        return s.nextLine();
+    }
 
+    public static boolean esBalanceado(Stack stack1, String cadena, boolean balanceado) {
         for (int i = 0; i < cadena.length(); i++) {
+            if (cadena.charAt(i) == ')' || cadena.charAt(i) == ']' || cadena.charAt(i) == '}') {
+                if (stack1.isEmpty()) {
+                    balanceado = false;
+                    break;
+                }
+            }
             if (cadena.charAt(i) == '(') stack1.push('(');
+            if (cadena.charAt(i) == '[') stack1.push('[');
+            if (cadena.charAt(i) == '{') stack1.push('{');
+
             if (cadena.charAt(i) == ')') {
-                //stack1.push(')');
-                stack1.search(')');
+                if (stack1.peek().equals('(')) stack1.pop();
+            }
+            if (cadena.charAt(i) == ']') {
+                if (stack1.peek().equals('[')) stack1.pop();
+            }
+            if (cadena.charAt(i) == '}') {
+                if (stack1.peek().equals('{')) stack1.pop();
             }
         }
 
-        System.out.println(stack1);
+        if (!stack1.isEmpty()) balanceado = false;
+
+        return balanceado;
+    }
+
+    public static void main(String[] args) {
+        Stack stack1 = new Stack();
+
+        boolean balanceado = true;
+
+        Scanner s = new Scanner(System.in);
+
+        String cadena = introducirCadena(s);
+
+        balanceado = esBalanceado(stack1, cadena, balanceado);
+
+        if (balanceado) System.out.println("La cadena está bien parentizada");
+        else System.out.println("La cadena no está bien parentizada");
     }
 }
