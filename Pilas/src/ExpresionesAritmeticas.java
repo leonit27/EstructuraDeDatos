948cb44b1ce8a5b9ca5ce733a5c9e596021cb87a
Leonardo <leonit@alu.edu.gva.es>
2025-01-14 09:35:32 +0100

    ExpresionesAritmeticas culminado

--- Pilas/src/ExpresionesAritmeticas.java
+++ Pilas/src/ExpresionesAritmeticas.java
@@ -2,47 +2,58 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class ExpresionesAritmeticas {
-    public static void main(String[] args) {
-        Stack<Character> stack1 = new Stack();
-
-        Scanner s = new Scanner(System.in);
-
+    public static String introducirExpresion(Scanner s) {
         System.out.print("Introduce una expresion con notación postfija: ");
         String expresion = s.nextLine();
 
-        expresion = expresion.replaceAll(" ", "");
+        return expresion.replaceAll(" ", "");
+    }
 
+    public static void evaluarExpresion (String expresion, Stack<Integer> stack1) {
         int i = 0;
 
-        while (i < expresion.length() - 1) {
-            while (Character.isDigit(expresion.charAt(i))) {
-                stack1.push(expresion.charAt(i));
+        if (expresion.length() == 1) {
+            stack1.push(Character.getNumericValue(expresion.charAt(i)));
+            System.out.println(stack1);
+        } else {
+            while (i < expresion.length()) {
+                while (Character.isDigit(expresion.charAt(i))) {
+                    stack1.push(Character.getNumericValue(expresion.charAt(i)));
+                    i++;
+                }
+
+                int num2 = stack1.pop();
+                int num1 = stack1.pop();
+
+                switch(expresion.charAt(i)) {
+                    case '+':
+                        stack1.push(num1 + num2);
+                        break;
+                    case '-':
+                        stack1.push(num1 - num2);
+                        break;
+                    case '*':
+                        stack1.push(num1 * num2);
+                        break;
+                    case '/':
+                        stack1.push(num1 / num2);
+                        break;
+                    default:
+                        break;
+                }
                 i++;
             }
-
-            System.out.println(stack1);
-
-            char num2 = stack1.pop();
-            char num1 = stack1.pop();
-
-            switch(expresion.charAt(i)) {
-                case '+':
-                    stack1.push((char) (num1 + num2));
-                    break;
-                case '-':
-                    //stack1.push(num1 - num2);
-                    break;
-                case '*':
-                    //stack1.push(num1 * num2);
-                    break;
-                case '/':
-                    //stack1.push(num1 / num2);
-                    break;
-                default:
-                    break;
-            }
-
             System.out.print(stack1);
         }
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> stack1 = new Stack();
+
+        Scanner s = new Scanner(System.in);
+
+        String expresion = introducirExpresion(s);
+
+        evaluarExpresion(expresion, stack1);
     }
 }
